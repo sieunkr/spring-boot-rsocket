e5a983088a21a3a54bdf96748452f6bd82d6ca70
Eddy.Kim <sieunkr@gmail.com>
2019-10-03 21:44:40 +0900

    ㅇㅇ

--- spring-boot-rsocket-basic/server/src/main/java/com/example/demo/SimpleRSocketImpl.java
+++ spring-boot-rsocket-basic/server/src/main/java/com/example/demo/SimpleRSocketImpl.java
@@ -4,6 +4,7 @@ import io.rsocket.AbstractRSocket;
 import io.rsocket.Payload;
 import io.rsocket.util.DefaultPayload;
 import lombok.extern.slf4j.Slf4j;
+import org.reactivestreams.Publisher;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -13,19 +14,19 @@ import reactor.core.publisher.Mono;
 public class SimpleRSocketImpl extends AbstractRSocket {
 
     @Override
-    public Mono<Payload> requestResponse(Payload payload) {
-
-        log.info(payload.getDataUtf8());
-
-        return Mono.just(DefaultPayload.create("단 하나의 데이터"));
-    }
-
-    @Override
     public Mono<Void> fireAndForget(Payload payload){
 
         System.out.println("server received");
 
         return Mono.empty();
+    }
+
+    @Override
+    public Mono<Payload> requestResponse(Payload payload) {
+
+        log.info(payload.getDataUtf8());
+
+        return Mono.just(DefaultPayload.create("단 하나의 데이터"));
     }
 
     @Override
@@ -38,4 +39,11 @@ public class SimpleRSocketImpl extends AbstractRSocket {
                 DefaultPayload.create("두번째 데이터 스트림"));
     }
 
+    @Override
+    public Flux<Payload> requestChannel(Publisher<Payload> payloads) {
+        log.info("테스트");
+        return null;
+    }
+
 }
+
